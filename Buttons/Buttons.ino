7d0eba50d73c629c22cab53f9a75a3f539fd84df
JackMGaw <geophphg@gmail.com>
2026-03-29 12:18:27 -0500

    Update Buttons.ino
    added debounce for button releases, fixed small error with flagHeld variable and setting prevRead

--- Buttons/Buttons.ino
+++ Buttons/Buttons.ino
@@ -1,9 +1,10 @@
 #define BUTTON1 19
 #define BUTTON2 20
 #define LED 1
+
 typedef unsigned int uint;            //I did this cause I hate writing out the whole data type
 typedef unsigned long ulong; 
-
+  
 const ulong debounceDelay = 50;       //50ms to wait for button debounce
 
 void setup() {
@@ -14,53 +15,56 @@ void setup() {
 }
 
 void loop() {
-  if(isButton1Pressed() && isButton2Pressed()){                //Only detects a SINGLE button press. Holding the button will not be interpretted as additional presses
-    Serial.println("Pressed");          
+  digitalWrite(LED, HIGH);
+  if(isButton1Pressed() || isButton2Pressed()){
     digitalWrite(LED, LOW);
     delay(3000);
-    }
-  else{
-    Serial.println("Not Pressed");}
-    digitalWrite(LED, HIGH);
+  }
+  
 }
 
-int isButton1Pressed(){
-  static ulong hitTime = 0;           //Time when a Button read is LOW (pressed)   
+int isButton1Pressed(){               
+  static ulong hitTime = 0;           //Time when a transition HIGH to LOW occurs
+  static ulong releaseTime = 0;       //Time when a transition LOW to HIGH occurs
   static int heldFlag = 0;            //Flag for knowing if button is being held down
   static int prevRead = HIGH;         //Reading of button from previous function call
   int Read = digitalRead(BUTTON1);    //Current reading of the button
-  if (Read == LOW && prevRead == HIGH) {hitTime = millis();}   //Record time when button goes from HIGH to LOW
-  
+  int result = 0;                     //Result to be returned at the end
+ 
+  if (Read == LOW && prevRead == HIGH) {hitTime = millis();}        //Rising edge, button press logic goes HIGH to LOW
   if (Read == LOW && prevRead == LOW) {             
-    if ((millis() - hitTime > debounceDelay) && !heldFlag) {
-      return 1;                       //Returns 1, the button has been pressed
-      heldFlag = 1;                   //flag for if button continues being held
+    if ((millis() - hitTime > debounceDelay) && !heldFlag) {        //Debounce delay for button release
+      heldFlag = 1;                                                 //Flag for indicating button is held
+      result = 1;                                                   
     }
-  return 0;
   }
-
-  if (Read == HIGH && prevRead == LOW) {heldFlag = 0;}    //Falling edge, button no longer held
+  if (Read == HIGH && prevRead == LOW) {releaseTime = millis();}    //Falling edge, button release logic goes LOW to HIGH
+  if (Read == HIGH && prevRead == HIGH){
+      if(millis() - releaseTime > debounceDelay) {heldFlag = 0;}    //Debounce delay for button release
+  }
   prevRead = Read;                                        //Set current read to prevRead for next function call
-  return 0;
+  return result;                                          //returns 1, button has been pressed; returns 0, button has not been pressed or is still being held
 }
 
-int isButton2Pressed(){
-  static ulong hitTime = 0;           //Time when a Button read is LOW (pressed)   
+int isButton2Pressed(){               
+  static ulong hitTime = 0;           //Time when a transition HIGH to LOW occurs
+  static ulong releaseTime = 0;       //Time when a transition LOW to HIGH occurs
   static int heldFlag = 0;            //Flag for knowing if button is being held down
   static int prevRead = HIGH;         //Reading of button from previous function call
   int Read = digitalRead(BUTTON2);    //Current reading of the button
-  if (Read == LOW && prevRead == HIGH) {hitTime = millis();}   //Record time when button goes from HIGH to LOW
-  
+  int result = 0;                     //Result to be returned at the end
+ 
+  if (Read == LOW && prevRead == HIGH) {hitTime = millis();}        //Rising edge, button press logic goes HIGH to LOW
   if (Read == LOW && prevRead == LOW) {             
-    if ((millis() - hitTime > debounceDelay) && !heldFlag) {
-      return 1;                       //Returns 1, the button has been pressed
-      heldFlag = 1;                   //flag for if button continues being held
+    if ((millis() - hitTime > debounceDelay) && !heldFlag) {        //Debounce delay for button release
+      heldFlag = 1;                                                 //Flag for indicating button is held
+      result = 1;                                                   
     }
-  return 0;
   }
-
-  if (Read == HIGH && prevRead == LOW) {heldFlag = 0;}    //Falling edge, button no longer held
+  if (Read == HIGH && prevRead == LOW) {releaseTime = millis();}    //Falling edge, button release logic goes LOW to HIGH
+  if (Read == HIGH && prevRead == HIGH){
+      if(millis() - releaseTime > debounceDelay) {heldFlag = 0;}    //Debounce delay for button release
+  }
   prevRead = Read;                                        //Set current read to prevRead for next function call
-  return 0;
+  return result;                                          //returns 1, button has been pressed; returns 0, button has not been pressed or is still being held
 }
-
