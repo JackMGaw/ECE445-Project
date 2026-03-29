e8c4f25ddd4b189b5e68e1c88f822e9a7e3aa03e
JackMGaw <geophphg@gmail.com>
2026-03-28 20:08:25 -0500

    Update Buttons
    Finished code for button inputs. One press = one output, no mater how long it is held. Resets upon release of button.

--- Buttons/Buttons.ino
+++ Buttons/Buttons.ino
@@ -1,40 +1,66 @@
-#define BUT1 19
-#define BUT2 20
+#define BUTTON1 19
+#define BUTTON2 20
 #define LED 1
-typedef unsigned int uint;        //I did this cause I hate writing out the whole data type
+typedef unsigned int uint;            //I did this cause I hate writing out the whole data type
 typedef unsigned long ulong; 
 
-const ulong debounceDelay = 50;   //50ms to wait for button debounce
-int prevBut1State = HIGH;       //Sets last stable state to HIGH, since this is when button is not pressed, static so it persists between repeated function calls
-ulong int prevBut1Time = 0;
+const ulong debounceDelay = 50;       //50ms to wait for button debounce
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BUT1, INPUT_PULLUP);    //Sets it as pullup, when unconnected and experinceing high impedance, pin reads HIGH, when connected to ground the pin read LOW
+  pinMode(BUTTON1, INPUT_PULLUP);     //Sets it as pullup, when unconnected and experinceing high impedance, pin reads HIGH, when connected to ground the pin read LOW
+  pinMode(BUTTON2, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  if(isBut1Pressed()){
-    Serial.println("Pressed");
+  if(isButton1Pressed() && isButton2Pressed()){                //Only detects a SINGLE button press. Holding the button will not be interpretted as additional presses
+    Serial.println("Pressed");          
     digitalWrite(LED, LOW);
+    delay(3000);
     }
   else{
     Serial.println("Not Pressed");}
     digitalWrite(LED, HIGH);
 }
 
-int isBut1Pressed(){
-  ulong currentTime = millis();
-  int but1Reading = digitalRead(BUT1);
-  static int currentBut1State;
-  if(but1Reading != lastStableState) {lastDebounce = millis();}  //millis calls the time in ms since the program has started, returns ulong
+int isButton1Pressed(){
+  static ulong hitTime = 0;           //Time when a Button read is LOW (pressed)   
+  static int heldFlag = 0;            //Flag for knowing if button is being held down
+  static int prevRead = HIGH;         //Reading of button from previous function call
+  int Read = digitalRead(BUTTON1);    //Current reading of the button
+  if (Read == LOW && prevRead == HIGH) {hitTime = millis();}   //Record time when button goes from HIGH to LOW
   
-  if(millis() - prevBut1Time > debounceDelay) {
-    if(but1Reading == LOW && lastStableState == HIGH) {
-      
+  if (Read == LOW && prevRead == LOW) {             
+    if ((millis() - hitTime > debounceDelay) && !heldFlag) {
+      return 1;                       //Returns 1, the button has been pressed
+      heldFlag = 1;                   //flag for if button continues being held
+    }
+  return 0;
   }
-  lastStableState = but1Reading;
+
+  if (Read == HIGH && prevRead == LOW) {heldFlag = 0;}    //Falling edge, button no longer held
+  prevRead = Read;                                        //Set current read to prevRead for next function call
+  return 0;
+}
+
+int isButton2Pressed(){
+  static ulong hitTime = 0;           //Time when a Button read is LOW (pressed)   
+  static int heldFlag = 0;            //Flag for knowing if button is being held down
+  static int prevRead = HIGH;         //Reading of button from previous function call
+  int Read = digitalRead(BUTTON2);    //Current reading of the button
+  if (Read == LOW && prevRead == HIGH) {hitTime = millis();}   //Record time when button goes from HIGH to LOW
+  
+  if (Read == LOW && prevRead == LOW) {             
+    if ((millis() - hitTime > debounceDelay) && !heldFlag) {
+      return 1;                       //Returns 1, the button has been pressed
+      heldFlag = 1;                   //flag for if button continues being held
+    }
+  return 0;
+  }
+
+  if (Read == HIGH && prevRead == LOW) {heldFlag = 0;}    //Falling edge, button no longer held
+  prevRead = Read;                                        //Set current read to prevRead for next function call
   return 0;
 }
 
